b52ecb8e8d8c9c21923cb41ecd0d2c8065eac302
nanwu <nanbytesflow@gmail.com>
2016-01-16 18:04:16 -0500

    more arithmetic funcs

--- arithmetic.ml
+++ arithmetic.ml
@@ -6,11 +6,23 @@ let sqrt ?(est=1) num =
 
 let is_prime n = 
   let rec check_prime_divisor i =
-    if (6*i - 1) > sqrt n 
-    if n % (6*i - 1) = 0 || n % (6*i + 1) = 0
-      then False
+    if (6*i - 1) > sqrt n then True
+    else if n % (6*i - 1) = 0 || n % (6*i + 1) = 0 then False
     else check_prime_divisor (i + 1)
   in 
   if n % 2 = 0 then False
   else if n % 3 = 0 then False
-  else 
+  else check_prime_divisor 1 
+
+
+let gcd x y = 
+  if y = 0 then x
+  else gcd y (x mod y)
+
+
+let prime_factor n = 
+  let rec helper d n =
+    if n mod d = 0 then d :: helper d (n / d) else helper (d+1) n
+  in helper 2 n
+
+
